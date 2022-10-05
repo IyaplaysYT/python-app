6f3c018a4cc4d23e4a332a24f0225f9d13e89b06
TheGiantNinja <73761819+yasserfds@users.noreply.github.com>
2022-10-05 15:17:51 +0200

    Update main.py

--- Python-Problems/main.py
+++ Python-Problems/main.py
@@ -25,6 +25,6 @@ Complex => Syntax: X + Yj
 width = 15
 height = 2 * 5
 print(height * width)
-# When height is multiplayed to height and devided by 5 it will print a new result
+# When height is multiplayed to width and devided by 5 it will print a new result
 if(height * width):
     print(height * width / 5)
