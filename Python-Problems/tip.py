27dc1bd4b9447aedc1c0acca41ac0c6f494c39c9
TheGiantNinja <yasserfedsi34@gmail.com>
2022-12-30 20:55:18 +0100

    done

--- Python-Problems/tip.py
+++ Python-Problems/tip.py
@@ -1,1 +1,16 @@
 # Don't do this
+a = 20
+b = 10
+
+if (a > b): 
+    print ("Greatest is ", a)
+else:
+    print ("Greatest is ", b)
+# Greatest is 20
+
+# Instead of this
+a = 20
+b = 10
+res = a if (a > b) else b
+print("Greatest is ", res)
+# Greatest value is 20
